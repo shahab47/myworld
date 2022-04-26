78baabc4e4cf35f76f0dbe469932728b160a9ae5
shkh <csshahab47@gmail.com>
2022-04-26 14:05:21 +0430

    my first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a my first commit on git
---
+++ pages/index.js
@@ -0,0 +1,34 @@
+
+
+export default function Home() {
+  return (
+    <div className="container">
+
+      اول یک بار یزن 
+      git --version
+      ببین که نصبه روی سیستمت یا نه 
+
+
+      git init 
+      git add -A
+      اگر پیام زیر برات اومد
+      warning: LF will be replaced by CRLF in pages/index.js.
+      The file will have its original line endings in your working directory
+       باید بزنی 
+       git config core.autocrlf true
+
+      git config --global user.email "csshahab47@gmail.com" 
+      git config --global user.name "shkh" 
+      git add -A 
+      git commit -m "my first commit"
+
+      git config --global user.email "you@example.com"
+      git config --global user.name "Your Name"
+      اول یک ریپازیتوری در ریموت خودت بساز 
+      بعد اون لینک https که میده از ریپازیتوری رو بردار و بیار 
+      برای پوش کردن 
+      git push adress branchname 
+      
+    </div>
+  )
+}
