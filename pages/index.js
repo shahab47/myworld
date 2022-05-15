f4f61462a590e900c2d132626d71f21cb326d0ee
shahab47 <csshahab47@gmail.com>
2022-05-15 15:33:41 +0430

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,23 @@
 export default function Home() {
   return (
     <div className="container">
+    
+    
+    
+    …or create a new repository on the command line
+echo "# reactToDoList" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/shahab47/reactToDoList.git
+git push -u origin main
+…or push an existing repository from the command line
+git remote add origin https://github.com/shahab47/reactToDoList.git
+git branch -M main
+git push -u origin main
+…or import code from another repository
+You can initialize this repository with code from a Subversion, Mercurial, or TFS project.
 
       اول یک بار یزن 
       git --version
