4e63e4299431d6aabdda321edb875bc373618cbf
shahab47 <csshahab47@gmail.com>
2022-06-14 10:23:10 +0430

    Update wysiwyg.js

--- pages/cmf/wysiwyg.js
+++ pages/cmf/wysiwyg.js
@@ -1,13 +1,11 @@
 ویزی ویگ 
 در کل به ادیتور هایی که اجازه میدن متن رو بنویسی میگن ویزی ویگ 
 مثل
-froala editor
 Tinymce
-Ckeditor
 Vev
 Editor js
 
-
+https://quilljs.com/docs/quickstart/
 https://slatejs.org
 https://youtube.com/playlist?list=PLN3n1USn4xllb05dQVmRbVtGP2aM4seVq
 https://tiptap.dev/
@@ -16,3 +14,6 @@ Draftjs
 Editorjs
 فیلم مقایسه اینها با هم
 https://youtu.be/MyiBAziEWUA
+
+https://prosemirror.net/examples/codemirror/
+این یکی کد ادیتور داره 
